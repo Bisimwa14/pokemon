e101ed64995d3e96a9302d76e7e3e6f2688117ac
Bisimwa14 <chiruzabisimwa@outlook.fr>
2026-04-18 14:50:21 +0200

    Fetching data

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { PokemonType } from "./types/pokemon";
 
 export default function Index() {
+  const [pokemmons, setPokemons] = useState<PokemonType[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=10",
+      );
+      const data = await response.json();
+      // console.log(data);
+
+      const detaildPokemons = await Promise.all(
+        data.results.map(async (pokemon: PokemonType) => {
+          const res = await fetch(pokemon.image);
+          const dto = await res.json();
+          return {
+            name: pokemon.name,
+            img: dto.sprites.front_default, //main sprite
+          };
+        }),
+      );
+      console.log(detaildPokemons);
+
+      setPokemons(detaildPokemons);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  }
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Racine App</Text>
-    </View>
+    <ScrollView>
+      {pokemmons.map((pokemon) => (
+        <View key={pokemon?.name}>
+          <Text>{pokemon?.name}</Text>
+          <Text>{pokemon?.image}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
